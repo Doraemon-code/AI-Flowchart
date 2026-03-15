858fe0497b9e00b532eeeb77cc4cf2692a202b97
Doraemon-code <1002458399@qq.com>
2026-03-15 17:21:24 +0800

    bug：字体缓存块路径变量类型声明，否则影响编译

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 
 // 配置 Excalidraw 从本地加载资源（字体等）
 // 设置后 Excalidraw 会从 /fonts/ 路径加载字体文件
+declare global {
+  interface Window {
+    EXCALIDRAW_ASSET_PATH: string;
+  }
+}
+
 window.EXCALIDRAW_ASSET_PATH = '/'
 
 import './index.css'
