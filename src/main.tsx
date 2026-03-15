f0735cafeacc3ca40787cfac1eb43cb7145c831b
Doraemon-code <1002458399@qq.com>
2026-03-15 16:43:48 +0800

    feat： 1. 增加本地手写字体缓存

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+// 配置 Excalidraw 从本地加载资源（字体等）
+// 设置后 Excalidraw 会从 /fonts/ 路径加载字体文件
+window.EXCALIDRAW_ASSET_PATH = '/'
+
 import './index.css'
 import App from './App.tsx'
 
